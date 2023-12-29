9f03135e0e657c8075571d02cc4bd377db9b7ab2
HasibX2000 <hasiburbd24@gmail.com>
2023-12-30 00:57:20 +0600

    Dark Mode Added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,15 +6,15 @@ const Footer = () => {
   console.log(date);
   return (
     <footer>
-      <footer className="z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <footer className="z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 text-center">
+        <span className="text-sm text-gray-500  sm:text-center dark:text-gray-400">
           &copy; {date} &nbsp;
-          <a to="/" className="hover:underline">
+          <Link to="/" className="hover:underline">
             CineDB
-          </a>
+          </Link>
           . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 text-center justify-center">
           <li>
             <Link
               to="https://facebook.com/HasibX2000"
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/logo.png';
 
 const Header = () => {
   const [hidden, setHidden] = useState(true);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem('darkMode')) || false
+  );
+  useEffect(() => {
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
   const activeClass =
     'block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500';
   const inactiveClass =
@@ -18,15 +29,52 @@ const Header = () => {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src={Logo} className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              CineDB
+            </span>
           </NavLink>
           <div className="flex md:order-2">
+            {/* Dark Mode  */}
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              id="theme-toggle"
+              type="button"
+              className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-2"
+            >
+              {darkMode ? (
+                <svg
+                  id="theme-toggle-dark-icon"
+                  className=" w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              ) : (
+                <svg
+                  id="theme-toggle-light-icon"
+                  className=" w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              )}
+            </button>
+            {/* Dark Mode  */}
             <button
               onClick={() => setHidden(!hidden)}
               type="button"
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-2"
             >
               <svg
                 className="w-5 h-5"
@@ -45,6 +93,7 @@ const Header = () => {
               </svg>
               <span className="sr-only">Search</span>
             </button>
+
             <div className="relative hidden md:block">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
@@ -71,6 +120,7 @@ const Header = () => {
                 placeholder="Search..."
               />
             </div>
+
             <button
               data-collapse-toggle="navbar-search"
               onClick={() => setHidden(!hidden)}
@@ -97,6 +147,7 @@ const Header = () => {
               </svg>
             </button>
           </div>
+
           <div
             className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
               hidden ? 'hidden' : ''
